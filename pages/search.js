a07ea8a656317e468a090948efbff6a32bfa5ec4
mukeshswamy <59552894+mukeshswamy@users.noreply.github.com>
2021-12-24 06:54:00 +0530

    push

---
+++ pages/search.js
@@ -0,0 +1,16 @@
+import Head from "next/head"
+import Header from "../components/Header"
+function Search() {
+    return (
+        <div>
+            <Head>
+                <title>Search Results</title>
+            </Head>
+            {/* Header */}
+            <Header/>
+            {/* Search results */}
+        </div>
+    )
+}
+
+export default Search
